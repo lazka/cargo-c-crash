a52ce1559ef2e2796f2bb37772a3a528fe53e8ae
Christoph Reiter <reiter.christoph@gmail.com>
2025-02-13 14:24:57 +0100

    use --meson-paths
    we dropped a patch in MSYS2 which partly did what meson-paths does

--- repro_dovi_tool.sh
+++ repro_dovi_tool.sh
@@ -5,10 +5,10 @@ set -e
 dist=$(pwd)/dist_dovi_tool
 cd dovi_tool
 cargo fetch
-cargo cbuild --release --manifest-path dolby_vision/Cargo.toml
-cargo cinstall --destdir $dist --manifest-path dolby_vision/Cargo.toml
+cargo cbuild --meson-paths --release --manifest-path dolby_vision/Cargo.toml
+cargo cinstall --meson-paths --destdir $dist --manifest-path dolby_vision/Cargo.toml
 # This makes things work, by using dlltool to re-create the import lib
-# dlltool.exe  -D$dist/bin/dovi.dll -d$dist/lib/dovi.def -l$dist/lib/libdovi.dll.a
+# dlltool.exe  -D$dist/bin/libdovi.dll -d$dist/lib/dovi.def -l$dist/lib/libdovi.dll.a
 cd ..
 PATH="$dist/bin:$PATH"
 cd dovi_tool/dolby_vision/examples
--- repro_libimagequant.sh
+++ repro_libimagequant.sh
@@ -5,10 +5,10 @@ set -e
 dist=$(pwd)/dist_libimagequant
 cd libimagequant
 cargo fetch
-cargo cbuild --release --manifest-path imagequant-sys/Cargo.toml
-cargo cinstall --destdir $dist --manifest-path imagequant-sys/Cargo.toml
+cargo cbuild --meson-paths --release --manifest-path imagequant-sys/Cargo.toml
+cargo cinstall --meson-paths --destdir $dist --manifest-path imagequant-sys/Cargo.toml
 # This makes things work, by using dlltool to re-create the import lib
-# dlltool.exe  -D$dist/bin/imagequant.dll -d$dist/lib/imagequant.def -l$dist/lib/libimagequant.dll.a
+# dlltool.exe  -D$dist/bin/libimagequant.dll -d$dist/lib/imagequant.def -l$dist/lib/libimagequant.dll.a
 cd ..
 PATH="$dist/bin:$PATH"
 cc crash.c -limagequant -L"$dist/lib" -I"$dist/include" -o crash.exe
